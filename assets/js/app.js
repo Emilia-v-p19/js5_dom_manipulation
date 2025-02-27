1fdf1ae3a85863a450bf53a19f74f972d4733ccd
Emilia-v-p19 <emilia@pretsch.dk>
2025-02-27 13:39:56 +0100

    js light and dark theme

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,28 @@
 // Opgave 1: sørg for at man ved klik på ☼/◯ (findes i nav > ul > .toggleColormode) skifter imellem light og darkmode. Se at <body> har en class der matcher det i style.css
 
+//fang toggle-element
+const toggleEl =document.querySelector(".toggleColormode");
+
+// fang body
+const bodyEl =document.querySelector("body");
+
+// lav eventListen paa toggleknap
+toggleEl.addEventListener("click",function(){
+    if(bodyEl.classList.contains("light")) {
+        bodyEl.classList.remove("light")
+        bodyEl.classList.add("dark")
+        toggleEl.innerHTML = "◯ ";
+    } else{
+        bodyEl.classList.remove("dark")
+        bodyEl.classList.add("light")
+        toggleEl.innerHTML = "☼";
+        localStorage.setItem("theme","light")
+    }
+})
+
+
 // Opgave 2: sørg for at gemme det i localStorage så brugerens indstillinger gemmes
+
 
 // Opgave 3: Hver person har en <h4> inde i <main>. Den er dog meget dum og der står bare "Look at me". Du skal istedet opdatere <h4> med det der står i billedets alt-attribute. For den første person skal der altså stå: "Jim the Chef". Anvend for guds skyld loops/løkker til det!
 
